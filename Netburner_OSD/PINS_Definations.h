440c51a7180e812d469bf058497c5c297cd7f677
tmandal1988 <tmandal1988@gmail.com>
2014-08-13 15:03:04 -0400

    OSD code v1

---
+++ Netburner_OSD/PINS_Definations.h
@@ -0,0 +1,26 @@
+/*
+ * PINS_Definations.h
+ *
+ *  Created on: Aug 13, 2014
+ *      Author: Tanmay
+ */
+
+#ifndef PINS_DEFINATIONS_H_
+#define PINS_DEFINATIONS_H_
+
+#include <basictypes.h>
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void initPINS();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PINS_DEFINATIONS_H_ */
