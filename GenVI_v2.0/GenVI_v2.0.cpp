7a3e09467a7a2667450f629e2b8e8ce65872bf02
Tanmay <tmandal1988@gmail.com>
2015-03-11 09:56:11 -0400

    Tested on PIRAT, OSD still not working

--- GenVI_v2.0/GenVI_v2.0.cpp
+++ GenVI_v2.0/GenVI_v2.0.cpp
@@ -48,7 +48,7 @@ void UserMain(void * pd);
 //void SetIntc(int intc, long func, int source, int level);
 }
 
-const char *AppName = "GenVI Avionics Netburner Firmware v2";
+const char *AppName = "GenVI Avionics Netburner Firmware v2.1";
 
 static uint16_t pilot_input[4] = { 0 }; //0.Throttle, 1.Aileron, 2.Elevator,3.Rudder
 static char GPS_packet[140] = { 0 };
